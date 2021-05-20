d09e2da308698757bf35d24d8652a386ea0c47f2
oluwadan <danielloluwatosin@gmail.com>
2021-05-20 14:01:01 +0100

    techstar

---
+++ js/index.js
@@ -0,0 +1,93 @@
+document.getElementById("btn1").addEventListener("click", function () {
+    var box1=document.getElementById("test1");
+    if (box1.style.display == "none") {
+        box1.style.display = "block";
+    }else{
+        box1.style.display = "none";
+    }
+            
+});
+
+
+document.getElementById("btn2").addEventListener("click", function () {
+    var box1=document.getElementById("test2");
+    if (box1.style.display == "none") {
+        box1.style.display = "block";
+    }else{
+        box1.style.display = "none";
+    }
+            
+});
+
+document.getElementById("btn3").addEventListener("click", function () {
+    var box1=document.getElementById("test3");
+    if (box1.style.display == "none") {
+        box1.style.display = "block";
+    }else{
+        box1.style.display = "none";
+    }
+                    
+}); 
+        
+document.getElementById("btn4").addEventListener("click", function () {
+    var box1=document.getElementById("test4");
+    if (box1.style.display =="none") {
+        box1.style.display = "block";
+    }else{
+        box1.style.display = "none";
+    }
+});
+
+document.getElementById("btn5").addEventListener("click", function () {
+    var box1=document.getElementById("test5");
+    if (box1.style.display =="none") {
+        box1.style.display = "block";
+    }else{
+        box1.style.display = "none";
+    }
+});
+
+document.getElementById("btn6").addEventListener("click", function () {
+    var box1=document.getElementById("test6");
+    if (box1.style.display =="none") {
+        box1.style.display = "block";
+    }else{
+        box1.style.display = "none";
+    }
+});
+
+document.getElementById("btn7").addEventListener("click", function () {
+    var box1=document.getElementById("test7");
+    if (box1.style.display =="none") {
+        box1.style.display = "block";
+    }else{
+        box1.style.display = "none";
+    }
+});
+
+document.getElementById("btn8").addEventListener("click", function () {
+    var box1=document.getElementById("test8");
+    if (box1.style.display =="none") {
+        box1.style.display = "block";
+    }else{
+        box1.style.display = "none";
+    }
+});
+
+document.getElementById("btn9").addEventListener("click", function () {
+    var box1=document.getElementById("test9");
+    if (box1.style.display =="none") {
+        box1.style.display = "block";
+    }else{
+        box1.style.display = "none";
+    }
+});
+
+document.getElementById("btn10").addEventListener("click", function () {
+    var box1=document.getElementById("test10");
+    if (box1.style.display =="none") {
+        box1.style.display = "block";
+    }else{
+        box1.style.display = "none";
+    }
+});
